7b1e91e029815a2470a9024c00620a3ddb5f8223
jonnathan <jhonthancarlos@gmail.com>
2022-04-18 22:06:46 -0300

    fasendo os desaineis da pagina login

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,8 +1,22 @@
-import { TextInput, View } from "react-native"
+import { TextInput, View,StyleSheet } from "react-native"
+import React from "react"
+
+const styles = StyleSheet.create({
+    container:{
+        flexGrow:1,
+        justifyContent: 'center',
+        padding: 32,
+        backgroundColor: '#ff0', 
+    }
+    input: {
+        fontSize:24,
+    },
+})
+ 
 
 const LoginScreen: React.FC = () => {
 return(
-    <View>
+    <View style={styles.container}>
         <TextInput placeholder="nome do usuario"/>
     </View>
 )
