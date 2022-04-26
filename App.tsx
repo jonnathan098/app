0fb50bfac96a142e44d04f2c3d0e5fb3ab124ee5
jonnathan <jhonthancarlos@gmail.com>
2022-04-25 21:57:58 -0300

    crinado caminhos para a navegaçao

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import PaginaItem from './src/screens/item';
 import LoginScreen from './src/screens/Login';
 import PerfilScreen from './src/screens/Perfil';
 import TelaPricipal from './src/screens/TelaPricipal';
 
+
+const Stack = createNativeStackNavigator();
+
 const app: React.FC = () => {
   return(
-   <PaginaItem />
-    //<TelaPricipal/>
+    <NavigationContainer>{}</NavigationContainer>
   );
 };
 
 
 
 export default app;
+
+
